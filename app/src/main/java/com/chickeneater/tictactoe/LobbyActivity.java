c9f2a5a301058365d5c4af88b858012bd5b6fb59
Nithil <nithilaksha@gmail.com>
2018-11-09 17:26:39 -0800

    Refractored to AndroidX

--- app/src/main/java/com/chickeneater/tictactoe/LobbyActivity.java
+++ app/src/main/java/com/chickeneater/tictactoe/LobbyActivity.java
@@ -2,10 +2,10 @@ package com.chickeneater.tictactoe;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
--- app/src/main/java/com/chickeneater/tictactoe/core/data/BluetoothConnector.java
+++ app/src/main/java/com/chickeneater/tictactoe/core/data/BluetoothConnector.java
@@ -1,8 +1,8 @@
 package com.chickeneater.tictactoe.core.data;
 
-import android.arch.lifecycle.Lifecycle;
-import android.arch.lifecycle.LifecycleObserver;
-import android.arch.lifecycle.OnLifecycleEvent;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
